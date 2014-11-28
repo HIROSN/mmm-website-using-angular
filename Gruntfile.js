b9e2095a25d60f6f5c83e86dd48fa24e433d42eb
Hiroshi Nagata <hirosn@live.com>
2014-11-28 09:18:49 -0800

    Baseline Angular app. See if Travis passes.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,18 @@
 module.exports = function(grunt) {
   var srcFiles = [
     '**/*.js',
-    '!node_modules/**/*'
+    '!node_modules/**/*',
+    '!bower_components/**/*',
+    '!public/**/*'
   ];
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-jscs');
   grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-sass');
 
   grunt.initConfig({
     jshint: {
@@ -27,12 +33,59 @@ module.exports = function(grunt) {
 
     simplemocha: {
       src: ['test/unit/**/*.js', 'test/api/**/*.js']
+    },
+
+    clean: {
+      dev: {
+        src: ['public/']
+      }
+    },
+
+    copy: {
+      dev: {
+        cwd: 'app/',
+        src: ['**/*.html'],
+        expand: true,
+        dest: 'public/'
+      },
+
+      map: {
+        cwd: 'bower_components/angular/',
+        src: ['**/angular.min.js.map'],
+        expand: true,
+        dest: 'public/'
+      }
+    },
+
+    browserify: {
+      dev: {
+        src: [
+          'app/js/**/*.js',
+          'bower_components/angular/angular.min.js'
+        ],
+        dest: 'public/bundle.js',
+        options: {
+          transform: ['debowerify']
+        }
+      }
+    },
+
+    sass: {
+      dist: {
+        files: {
+          'public/stylesheet.css': 'app/css/stylesheet.scss'
+        }
+      }
     }
   });
 
   grunt.registerTask('default', [
     'jshint',
     'jscs',
-    'simplemocha'
+    'simplemocha',
+    'clean',
+    'copy',
+    'browserify',
+    'sass'
   ]);
 };
---
+++ app/js/client.js
@@ -0,0 +1,11 @@
+'use strict';
+
+require('angular');
+
+(function() {
+  var app = angular.module('meanMedianMode', []);
+
+  app.controller('MeanMedianModeController', function() {
+    console.log('TODO');//TODO
+  });
+})();
