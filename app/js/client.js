c607833c08d539d6af5790f4c49126f0d68c3b28
Hiroshi Nagata <hirosn@live.com>
2014-11-28 11:29:30 -0800

    Turning in Create a mean/median/mode website using angular assignment.

--- app/js/client.js
+++ app/js/client.js
@@ -5,7 +5,21 @@ require('angular');
 (function() {
   var app = angular.module('meanMedianMode', []);
 
-  app.controller('MeanMedianModeController', function() {
-    console.log('TODO');//TODO
-  });
+  app.controller('MeanMedianModeController', ['$http', function($http) {
+    var self = this;
+    self.numbers = '';
+    self.results = {};
+
+    this.post = function() {
+      $http.post('/api', {
+        numbers: self.numbers && self.numbers.split(',')
+      }).
+      success(function(data) {
+        self.results = data;
+      }).
+      error(function() {
+        self.results = {};
+      });
+    };
+  }]);
 })();
