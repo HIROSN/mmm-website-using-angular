6296c132f2af17a106c01e44a3038c9d1d24d39a
Hiroshi Nagata <hirosn@live.com>
2014-12-01 07:22:28 -0800

    Use custom directive.

--- app/js/client.js
+++ app/js/client.js
@@ -7,4 +7,11 @@ require('angular-resource');
   var app = angular.module('meanMedianMode', ['ngResource']);
   require('./services/mmm-service')(app);
   require('./controllers/mmm-controller')(app);
+
+  app.directive('mmmDisplay', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'mmm-display.html'
+    };
+  });
 })();
